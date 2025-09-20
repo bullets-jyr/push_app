4787d68f4824095ed86527e214d806c47da7b80e
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-09-20 14:20:53 +0900

    feat: Add second screen for notification information

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -103,4 +103,15 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
     add(NotificationsStatusChanged(settings.authorizationStatus));
   }
+
+  PushMessage? getMessageById(String pushMessageId) {
+    final exist = state.notifications.any(
+      (element) => element.messageId == pushMessageId,
+    );
+    if (!exist) return null;
+
+    return state.notifications.firstWhere(
+      (element) => element.messageId == pushMessageId,
+    );
+  }
 }
