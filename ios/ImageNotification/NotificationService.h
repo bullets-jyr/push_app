a69f0feffa81e622a6d500f27c5d4d49e88a9c4b
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-09-21 14:09:01 +0900

    feat: Allow images in notifications

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by 류지영 on 9/21/25.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
